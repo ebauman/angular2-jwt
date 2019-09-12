88cd360bdfc0f93410944631782b42805da2280e
Eamon Bauman <eamon@eamonbauman.com>
2019-09-12 10:56:49 -0700

    removed comments

--- index.ts
+++ index.ts
@@ -14,8 +14,6 @@ export interface JwtModuleOptions {
     tokenGetter?: () => string | null | Promise<string | null>;
     headerName?: string;
     authScheme?: string;
-    // whitelistedDomains?: Array<string | RegExp>;
-    // blacklistedRoutes?: Array<string | RegExp>;
     whitelistedDomains?: () => Array<string | RegExp> | Promise<Array<string | RegExp>>;
     blacklistedRoutes?: () => Array<string | RegExp> | Promise<Array<string | RegExp>>;
     throwNoTokenError?: boolean;
